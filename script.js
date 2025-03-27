6686bb04f4bb651f16ed349b4a24488255533d94
Kaito091023 <akirodump130@gmail.com>
2025-03-28 00:22:52 +0800

    script.js

--- script.js
+++ script.js
@@ -7,28 +7,28 @@ document.getElementById('scrollButton').onclick = function () {
 
 document.getElementById('scrollButton0').onclick = function () {
     window.scrollTo({
-        top: 680,
+        top: document.body.scrollHeight / 1,
         behavior: 'smooth'
     })
 };
 
 document.getElementById('scrollButton1').onclick = function () {
     window.scrollTo({
-        top: 1360,
+        top: document.body.scrollHeight / 2,
         behavior: 'smooth'
     })
 };
 
 document.getElementById('scrollButton2').onclick = function () {
     window.scrollTo({
-        top: 680,
+        top: document.body.scrollHeight / 3,
         behavior: 'smooth'
     })
 };
 
 document.getElementById('scrollButton3').onclick = function () {
     window.scrollTo({
-        top: document.body.scrollHeight / 1,
+        top: document.body.scrollHeight / 4,
         behavior: 'smooth'
     })
 };
